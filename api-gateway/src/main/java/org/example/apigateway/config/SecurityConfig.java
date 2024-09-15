6e251792ca16d80941ee38975bde28372553a81a
gau2209 <anhtuanvo2209@gmail.com>
2024-09-15 17:54:10 +0700

    api- gateway

---
+++ api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package org.example.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+       return httpSecurity.authorizeHttpRequests(au -> au.anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(oau -> oau.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
